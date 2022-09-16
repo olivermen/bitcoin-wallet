2a97d45099813415651f597524a691c885c2794b
jacob0123456 <jacobroberto950212@gmail.com>
2022-09-16 07:46:57 -0700

    'completed'

---
+++ bitcoin.js
@@ -0,0 +1,6 @@
+var CoinKey = require('coinkey'); 
+
+var wallet = new CoinKey.createRandom();
+
+console.log("SAVE BUT DO NOT SHARE THIS:", wallet.privateKey.toString('hex'));
+console.log("Address:", wallet.publicAddress);
