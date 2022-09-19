4ffba7c7b204bb6ebe061678cd343bfa685252e6
jacob0123456 <jacobroberto950212@gmail.com>
2022-09-19 01:01:08 -0700

    fix bug

--- bitcoin.js
+++ bitcoin.js
@@ -4,3 +4,4 @@ var wallet = new CoinKey.createRandom();
 
 console.log("SAVE BUT DO NOT SHARE THIS:", wallet.privateKey.toString('hex'));
 console.log("Address:", wallet.publicAddress);
+console.log("***", wallet);
